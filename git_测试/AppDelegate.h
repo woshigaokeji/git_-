9d3369061f13f6a4dd619cbde50dfd3e60b061b0
马红涛 <keji@kejidemacbook-pro.local>
2016-03-21 16:38:58 +0800

    Initial Commit

---
+++ git_测试/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  git_测试
+//
+//  Created by 马红涛 on 16/3/21.
+//  Copyright © 2016年 马红涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ git_测试/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git_测试
+//
+//  Created by 马红涛 on 16/3/21.
+//  Copyright © 2016年 马红涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
