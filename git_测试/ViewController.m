1cfa9f2084fbc6d6c952582a6c146a6143ded841
马红涛 <keji@kejidemacbook-pro.local>
2016-03-21 17:22:58 +0800

    在新的分支里面修改文件

--- git_测试/ViewController.m
+++ git_测试/ViewController.m
@@ -19,6 +19,8 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"haha");
     UIView *view = [[UIView alloc] init];
+    [self.view addSubview:view];
+    NSLog(@"我添加了一个新的分支!");
 }
 
 - (void)didReceiveMemoryWarning {
